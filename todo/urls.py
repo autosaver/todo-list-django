4593dbca47b224096e1efa56315828a897a86ce5
autosaver <adityasingh1235467890@gmail.com>
2020-07-08 11:43:07 +0530

    DEPLOY

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.baseview,name='baseview'),
+    path('', views.baseview, name='baseview'),
+    path('deltask/<int:todo_id>/', views.deltask, name='deletion')
 ]
